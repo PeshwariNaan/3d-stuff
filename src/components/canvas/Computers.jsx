9356f559265505c4665641b506823e376a3ad373
J.D. Browne <PeshwariNaan01@proton.me>
2023-03-28 18:52:44 +0200

    Modified navbar and hero components.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  return <div>Computers</div>;
+};
 
-export default Computers
+export default Computers;
